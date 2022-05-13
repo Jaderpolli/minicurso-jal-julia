81d4fceee6726cc7812d5654872c72a5a820918d
jaderpolli <jaderpolli99@gmail.com>
2022-05-13 09:36:42 -0300

    Organize the course order

---
+++ 9_lorenz_trajectory_gif.jl
@@ -0,0 +1,49 @@
+using DynamicalSystems, Plots, LaTeXStrings
+
+# Vamos criar um gif que simule a evolução do atrator de lorenz ao variar r
+# observando quatro condições iniciais indo aos seus estados assintóticos
+
+function main()
+    cores = [:red, :blue, :orange, :green] # uma cor pra plotar cada trajetória
+    tf = 100.0 # tempo final de integração
+    σ = 10.0
+    r = (1.0:0.1:28) # r varia de 5 a 28 com passo = 0.1
+    L = length(r) # limite superior do contador
+    β = 8/3
+    ϵ = 0.1 # parâmetro para definir condições iniciais
+
+    pasta  = "lorenz"
+    mkpath(pasta) # cria uma pasta onde salvaremos o gif
+    plt = plot()
+
+    anim = @animate for i = 1:L
+        println("% $(i/L)%")
+        # existem três pontos de equilíbrio no sistema:
+        # (0,0,0)
+        # (sqrt(b(r-1)),sqrt(b(r-1)),r-1)
+        # (-sqrt(b(r-1)),-sqrt(b(r-1)),r-1)
+        # para estudar como a estabilidade dos pontos de equilíbrio varia
+        # vamos iniciar três condições iniciais nos arredores dos pontos
+        # de equilíbrio e ver como as trajetórias evoluem!
+        u0s = [[ϵ, ϵ, ϵ], # primeira c.i. ao redor de (0,0,0)
+                [(β*(r[i]-1))^(1/2)+ϵ,(β*(r[i]-1))^(1/2)+ϵ,r[i]-1+ϵ],
+                [-(β*(r[i]-1))^(1/2)+ϵ,-(β*(r[i]-1))^(1/2)+ϵ,r[i]-1+ϵ],
+                [-1.4, 2.3, 9.3]
+            ]
+        for u0 in u0s
+            ds = Systems.lorenz(u0, σ = σ, ρ = r[i], β = β)
+            tr = Matrix(trajectory(ds, tf, dt = 0.01))
+            plt = plot!(tr[:,1], tr[:,2], tr[:,3],
+                xlabel = L"x(t)",
+                ylabel = L"y(t)",
+                zlabel = L"z(t)",
+                lw = 2,
+                xlims = (-20.0,20.0), ylims = (-25.0,25.0), zlims = (0.0,50.0),
+                label = false
+            )
+        end
+    end
+    gif(anim, string(pasta, "/trajetorias_lorenz.gif"), fps = 2)
+end
+
+main()
